06e81707bf5e23da1dda7099dd7dabafb89a2e98
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-09-16 12:12:25 +0600

    Initial commit

---
+++ Backend/JamilOnlineMart/src/main/java/com/ZahidHasanJamil/JamilOnlineMart/JamilOnlineMartApplication.java
@@ -0,0 +1,13 @@
+package com.ZahidHasanJamil.JamilOnlineMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamilOnlineMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamilOnlineMartApplication.class, args);
+	}
+
+}
---
+++ Backend/JamilOnlineMart/src/test/java/com/ZahidHasanJamil/JamilOnlineMart/JamilOnlineMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ZahidHasanJamil.JamilOnlineMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamilOnlineMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
