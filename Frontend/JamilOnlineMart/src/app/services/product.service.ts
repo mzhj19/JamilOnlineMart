c1529c5dd762447a949a7e503a64b9ed29cf28dd
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-09-17 17:51:09 +0600

    BackendFrontend: Search products by category dynamically

--- Frontend/JamilOnlineMart/src/app/services/product.service.ts
+++ Frontend/JamilOnlineMart/src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ import { Product } from '../common/product';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoryUrl = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -20,10 +22,23 @@ export class ProductService {
       map(response => response._embedded.products)
     );
   }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+
+    return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 }
 
 interface GetResponse {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
