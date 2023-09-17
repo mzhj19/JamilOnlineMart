470b1957f739b8f48a901fdf51b656fb0a99fd8e
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-09-17 21:24:59 +0600

    BackendFrontend: Search products by keyword

--- Frontend/JamilOnlineMart/src/app/components/product-list/product-list.component.ts
+++ Frontend/JamilOnlineMart/src/app/components/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent implements OnInit{
   products: Product[] = [];
   currentCategoryId: number = 1;
   currentCategoryName: string = "";
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -24,21 +25,42 @@ export class ProductListComponent implements OnInit{
   }
 
   listProducts() {
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
+    }
+  }
+
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+    
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+  
+  handleListProducts() {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+  
     if (hasCategoryId) {
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
-      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     }
     else {
       this.currentCategoryId = 1;
-      this.currentCategoryName = 'Books';
     }
     
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data;
       }
-    )
+    )    
   }
 }
--- Frontend/JamilOnlineMart/src/app/services/product.service.ts
+++ Frontend/JamilOnlineMart/src/app/services/product.service.ts
@@ -5,6 +5,7 @@ import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,28 +16,33 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+
   getProductList(theCategoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    return this.getProducts(searchUrl);
+  }
 
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
-      map(response => response._embedded.products)
-    );
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`;
+    return this.getProducts(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(map(response => response._embedded.products));
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
-
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
       map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
   }
 }
-
 interface GetResponseProductCategory {
   _embedded: {
     productCategory: ProductCategory[];
