6f396464c7107f592b4ca4168438b36e34a62b90
Md. Zahid Hasan Jamil <mzhj19@gmail.com>
2023-09-19 00:01:35 +0600

    Frontend: Cart details- quantity,increment/decrement,subtotal,total etc

--- Frontend/JamilOnlineMart/src/app/services/cart.service.ts
+++ Frontend/JamilOnlineMart/src/app/services/cart.service.ts
@@ -56,4 +56,28 @@ export class CartService {
     this.totalQuantity.next(totalQuantityValue);
     
   }
+
+  decrementQuantity(theCartItem: CartItem) {
+
+    theCartItem.quantity--;
+
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    }
+    else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(theCartItem: CartItem) {
+    // get index of item in the array
+    const itemIndex = this.cartItems.findIndex( tempCartItem => tempCartItem.id === theCartItem.id );
+
+    // if found, remove the item from the array at the given index
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+
+      this.computeCartTotals();
+    }
+  }
 }
